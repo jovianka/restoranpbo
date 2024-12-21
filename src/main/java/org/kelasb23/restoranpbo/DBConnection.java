21482ac33d968560635c5e81fd49d8c22f3c0496
jovianka <jovianpratama123@gmail.com>
2024-12-21 13:20:40 +0800

    add code from kingdodde, still broken

--- src/main/java/org/kelasb23/restoranpbo/DBConnection.java
+++ src/main/java/org/kelasb23/restoranpbo/DBConnection.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.kelasb23.restoranpbo;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 
 /**
  *
@@ -10,17 +12,18 @@ package org.kelasb23.restoranpbo;
  */
 
 import java.sql.*;
-import java.util.Map;
-        
+import java.util.*;
 
 public class DBConnection {
     static Connection connection;
 
     public static Connection getConnection() {
         try {
-            Map<String, String> env_variables = System.getenv();
-            String url = "jdbc:mysql://localhost:3306/restoran_pbo?user=inikasihnamausernya&password=inikasihpasswordnya";
-            connection = DriverManager.getConnection(url);
+            Dotenv dotenv = Dotenv.configure().load();
+            String user = dotenv.get("DB_USER");
+            String pass = dotenv.get("DB_PASSWORD");
+            String url = "jdbc:mysql://localhost:3306/restoran_pbo";
+            connection = DriverManager.getConnection(url, user, pass);
         } catch (SQLException err) {
             System.out.println("Error Connecting to Database");
             System.out.println(err.getMessage());
